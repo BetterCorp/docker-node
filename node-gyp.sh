931406c7f45441cfb571230bb6487e285e543455
Mitchell R <support@bettercorp.co.za>
2022-08-06 22:32:13 +0200

    Bash for node-gyp prepped as well

--- node-gyp.sh
+++ node-gyp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-apk add --update --no-cache python3
-ln -sf python3 /usr/bin/python
+if command -v apk &> /dev/null ; then apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python ; fi
+
 python3 -m ensurepip
 pip3 install --no-cache --upgrade pip setuptools
 
