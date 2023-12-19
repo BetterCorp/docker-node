8da3a7e78f63601dca87c57519d615d9a5e30c6c
mrinc <github@mrincworld.com>
2023-12-20 01:29:28 +0200

    Attempt to build node versions

--- node-gyp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if command -v apk &> /dev/null ; then apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python ; fi
-
-python3 -m ensurepip
-pip3 install --no-cache --upgrade pip setuptools
-
-npm install -g node-gyp
